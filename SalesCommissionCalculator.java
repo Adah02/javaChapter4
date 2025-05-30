3c159da337fc9f21b16a254bd3de6c74e4e64eb4
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-05-30 22:28:18 +0100

    Percentage commission on sales

--- SalesCommissionCalculator.java
+++ SalesCommissionCalculator.java
@@ -10,19 +10,19 @@ public class PercentageCommission {
 
 	double totalAmount = userInput;
 	int numberOfItems = 0;
-
-	double constantWeeklyCommission = 200;
-	double percentageCommission = (9.00 / 100);
-	double commissionFromSales = (totalAmount * percentageCommission);
-	double totalCommission = constantWeeklyCommission + commissionFromSales;
 	
 	while (userInput != -1){
 	numberOfItems++;
 	System.out.printf("Enter price of item %d: ", (numberOfItems +1));
 	userInput = input.nextDouble();
 	totalAmount += userInput;
-	
 	}
+
+	double constantWeeklyCommission = 200;
+	double percentageCommission = (9.00 / 100);
+	double commissionFromSales = (totalAmount * percentageCommission);
+	double totalCommission = constantWeeklyCommission + commissionFromSales;
+
 	System.out.printf("Your total commission for the week is $%.2f%nFor %d items.", totalCommission, numberOfItems);
 	}
 }
